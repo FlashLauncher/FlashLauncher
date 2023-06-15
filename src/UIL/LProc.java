ed2394ceb2aace84c48d35c404040cfb33e47d9f
illa4257 <48959534+illa4257@users.noreply.github.com>
2023-06-15 07:06:45 +0300

    Update LProc.java

--- src/UIL/LProc.java
+++ src/UIL/LProc.java
@@ -1,41 +1,14 @@
 package UIL;
 
-import java.util.ArrayList;
-
 public class LProc {
-    private final String text;
-    private final String[] vars;
+    private final Object o;
 
-    public LProc(String str) {
-        if (str.contains("=<<!:")) {
-            final int i = str.indexOf("=<<!:");
-            String s = str.substring(0, i);
-            final ArrayList<String> l = new ArrayList<>();
-            for (String an : str.substring(i + 5).split(","))
-                if (an.length() > 0)
-                    l.add(an);
-                else
-                    l.add(null);
-            text = s;
-            vars = l.toArray(new String[0]);
-        } else {
-            text = str;
-            vars = new String[0];
-        }
-    }
+    public LProc(final Object object) { o = object; }
 
-    public LProc(LangItem li) {
-        this(li.toString());
-    }
-
-    public String toString(Object... args) {
-        String r = text;
-        for (int i = 0; i < args.length && i < vars.length; i++) {
-            final String vn = vars[i];
-            if (vn == null)
-                continue;
-            r = r.replace("%" + vn + ";", args[i].toString());
-        }
+    public String toString(final Object... args) {
+        String r = o.toString();
+        for (int i = args.length - 1; i >= 0; i--)
+            r = r.replaceAll("%" + i, args[i].toString());
         return r;
     }
 }
