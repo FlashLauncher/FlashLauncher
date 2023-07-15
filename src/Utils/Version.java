c1ca42316ecb2b85bb61b583b3d75aa5ff5797cf
illa4257 <48959534+illa4257@users.noreply.github.com>
2023-07-15 05:14:11 +0300

    Update Version.java

--- src/Utils/Version.java
+++ src/Utils/Version.java
@@ -5,24 +5,24 @@ public class Version {
 
     public Version(final String ver) { v = ver; }
 
-    public boolean isCompatibility(final String ver) {
+    public final boolean isCompatibility(final String ver) {
         if (ver == null || ver.length() == 0)
             return true;
         for (final String sv : ver.split("\\|"))
             if (sv.startsWith("-")) {
                 if ((sv.length() == 1 || sv.equals("-*") || v.equals(sv.substring(1))))
                     return false;
-            } else if (sv.length() == 0 || sv.equals("*"))
+            } else if (sv.length() == 0 || sv.equals("*") || sv.equals(v))
                 return true;
         return true;
     }
 
     @Override
-    public boolean equals(final Object obj) {
+    public final boolean equals(final Object obj) {
         if (obj instanceof Version)
             return v.equals(((Version) obj).v);
         return false;
     }
 
-    @Override public String toString() { return v; }
+    @Override public final String toString() { return v; }
 }
