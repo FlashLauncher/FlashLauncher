f5d061823983cfd4ebedb457eeff7e91f467403d
illa4257 <48959534+illa4257@users.noreply.github.com>
2023-07-25 20:30:13 +0300

    Update market API

--- src/Launcher/Market.java
+++ src/Launcher/Market.java
@@ -1,14 +1,34 @@
 package Launcher;
 
-import Launcher.metas.Meta;
+import UIL.Lang;
 import UIL.base.IImage;
 
 public abstract class Market {
-    public abstract String getID();
-    public abstract String getName();
-    public IImage getIcon() { return null; }
-    public abstract Meta getByID(final String id);
+    private final String id;
+    private final Object n;
+    private final IImage i;
 
-    public abstract TaskGroup refresh();
-    public abstract Meta[] find(final String search);
+    public Market(final String id, final IImage icon) {
+        if (id == null)
+            throw new RuntimeException("ID is empty!");
+        this.id = id;
+        n = Lang.get("markets." + id + ".name");
+        i = icon;
+    }
+
+    public Market(final String id, final Object name, final IImage icon) {
+        if (id == null)
+            throw new RuntimeException("ID is empty!");
+        this.id = id;
+        n = name;
+        i = icon;
+    }
+
+    public final String getID() { return id; }
+    public final IImage getIcon() { return i; }
+    public Object getName() { return n; }
+
+    public abstract void checkForUpdates(final Meta... items);
+
+    public abstract Meta[] find(final String query);
 }
