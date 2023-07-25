b4db3b6d3a136ae8429d7ad4285515db33a96910
illa4257 <48959534+illa4257@users.noreply.github.com>
2023-07-25 20:30:21 +0300

    Create Meta.java

---
+++ src/Launcher/Meta.java
@@ -0,0 +1,25 @@
+package Launcher;
+
+import UIL.base.IImage;
+import Utils.Version;
+
+public abstract class Meta {
+    public static final int ICON_SIZE = 56;
+
+    private final String id;
+    String author;
+    Version ver;
+
+    public Meta(final String id, final Version version, final String author) { this.id = id; ver = version; this.author = author; }
+
+    public final String getID() { return id; }
+    public Version getVersion() { return ver; }
+    public final String getAuthor() { return author; }
+
+    public Object[] getCategories() { return null; }
+    public TaskGroup install() { return null; }
+
+    public abstract IImage getIcon();
+    public abstract Object getName();
+    public abstract Object getShortDescription();
+}
