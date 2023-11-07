307cd788f0190724a67783da8605f05cd5476a24
illa4257 <48959534+illa4257@users.noreply.github.com>
2023-11-07 06:01:24 +0200

    Update Version.java

--- src/Utils/Version.java
+++ src/Utils/Version.java
@@ -8,12 +8,30 @@ public class Version {
     public final boolean isCompatibility(final String ver) {
         if (ver == null || ver.isEmpty())
             return true;
-        for (final String sv : ver.split(","))
-            if (sv.startsWith("-")) {
-                if ((sv.length() == 1 || sv.equals("-*") || v.equals(sv.substring(1))))
-                    return false;
-            } else if (sv.isEmpty() || sv.equals("*") || sv.equals(v))
+        m:
+        for (String sv : ver.split(",")) {
+            final boolean m = sv.startsWith("-");
+            sv = sv.substring(1);
+
+            if (sv.startsWith("^")) {
+                sv = sv.substring(1);
+                final String[] l1 = sv.split("\\."), l2 = v.split("\\.");
+                final int l = Math.min(l1.length, l2.length);
+                for (int i = 0; i < l; i++)
+                    if (!l1[i].equals(l2[i]))
+                        try {
+                            if (Integer.parseInt(l1[i]) > Integer.parseInt(l2[i]))
+                                continue m;
+                        } catch (final Exception ex) {
+                            ex.printStackTrace();
+                            continue m;
+                        }
                 return true;
+            }
+
+            if (sv.isEmpty() || sv.equals("*") || sv.equals(v))
+                return !m;
+        }
         return true;
     }
 
