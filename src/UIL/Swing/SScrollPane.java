bbc9f0378f6e323676f134631f992f8a9dbf069d
illa4257 <48959534+illa4257@users.noreply.github.com>
2023-11-07 06:09:51 +0200

    Update SScrollPane.java

--- src/UIL/Swing/SScrollPane.java
+++ src/UIL/Swing/SScrollPane.java
@@ -5,11 +5,13 @@ import UIL.base.IColor;
 import UIL.base.IComponent;
 import UIL.base.IContainer;
 import UIL.base.IScrollPane;
+import Utils.Core;
 import Utils.RRunnable;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Area;
+import java.awt.geom.Rectangle2D;
 import java.awt.geom.RoundRectangle2D;
 
 public class SScrollPane extends JPanel implements IScrollPane {
@@ -27,19 +29,22 @@ public class SScrollPane extends JPanel implements IScrollPane {
                 sx = Math.min(Math.max(sx - e.getUnitsToScroll() * SSwing.MULTIPLIER, getWidth() - content.width()), 0);
             else
                 sy = Math.min(Math.max(sy - e.getUnitsToScroll() * SSwing.MULTIPLIER, getHeight() - content.height()), 0);
+            content.pos(sx, sy);
             repaint();
         });
     }
 
-    @Override protected void paintComponent(final Graphics graphics) {}
+    private Area a = null;
+
+    private long prev;
 
     @Override
-    protected void paintChildren(final Graphics graphics) {
+    protected void paintComponent(final Graphics graphics) {
         final SGraphics2D g = graphics instanceof SGraphics2D ? (SGraphics2D) graphics :
                 new SGraphics2D((Graphics2D) (graphics instanceof Graphics2D ? graphics : graphics.create()));
 
         final int br = borderRadius.run(), w = getWidth(), h = getHeight(), sx = this.sx, sy = this.sy;
-        final Area a = new Area(br > 0 ? new RoundRectangle2D.Double(0, 0, w, h, br, br) : new Rectangle(0, 0, w, h));
+        a = new Area(br > 0 ? new RoundRectangle2D.Double(0, 0, w, h, br, br) : new Rectangle(0, 0, w, h));
         g.setClip(a);
 
         final IContainer c = content;
@@ -77,14 +82,22 @@ public class SScrollPane extends JPanel implements IScrollPane {
             }
 
             a.intersect(new Area(new Rectangle(0, 0, rw, rh)));
-            g.setClip(a);
-            g.translate(sx, sy);
-            final Graphics2D sg = new SGraphics2D(g);
-            super.paintChildren(sg);
-            sg.dispose();
         }
 
-        g.dispose();
+        if (!(graphics instanceof Graphics2D))
+            g.dispose();
+    }
+
+    @Override
+    protected void paintChildren(final Graphics graphics) {
+        final SGraphics2D g = graphics instanceof SGraphics2D ? (SGraphics2D) graphics :
+                new SGraphics2D((Graphics2D) (graphics instanceof Graphics2D ? graphics : graphics.create()));
+
+        g.setClip(a);
+        super.paintChildren(g);
+
+        if (!(graphics instanceof Graphics2D))
+            g.dispose();
     }
 
     @Override public int width() { return getWidth(); }
@@ -98,84 +111,33 @@ public class SScrollPane extends JPanel implements IScrollPane {
     @Override public SScrollPane content(final IContainer container) {
         if (content != null)
             super.remove((Component) content.getComponent());
-        super.add((Component) container.getComponent());
+        final Component c = (Component) container.getComponent();
+        super.add(c);
         content = container;
-        sx = 0;
-        sy = 0;
+        sx = c.getX();
+        sy = c.getY();
         return this;
     }
 
-    @Override
-    public SScrollPane add(final IComponent component) {
-        content.add(component);
-        return this;
-    }
-
-    @Override
-    public SScrollPane add(final IComponent... components) {
-        content.add(components);
-        return this;
-    }
-
-    @Override
-    public SScrollPane remove(final IComponent component) {
-        content.remove(component);
-        return this;
-    }
-
-    @Override
-    public SScrollPane clear() {
-        content.clear();
-        return this;
-    }
-
-    @Override
-    public SScrollPane size(final int width, final int height) {
-        setSize(width, height);
-        return this;
-    }
-
-    @Override
-    public SScrollPane pos(final int x, final int y) {
-        setLocation(x, y);
-        return this;
-    }
-
-    @Override
-    public SScrollPane visible(final boolean visible) {
-        setVisible(visible);
-        return this;
-    }
-
+    @Override public SScrollPane add(final IComponent component) { content.add(component); return this; }
+    @Override public SScrollPane add(final IComponent... components) { content.add(components); return this; }
+    @Override public SScrollPane remove(final IComponent component) { content.remove(component); return this; }
+    @Override public SScrollPane clear() { content.clear(); return this; }
+    @Override public SScrollPane size(final int width, final int height) { setSize(width, height); return this; }
+    @Override public SScrollPane pos(final int x, final int y) { setLocation(x, y); return this; }
+    @Override public SScrollPane visible(final boolean visible) { setVisible(visible); return this; }
     @Override public SScrollPane focus() { requestFocus(); return this; }
-
-    @Override
-    public SScrollPane borderRadius(final RRunnable<Integer> borderRadius) {
-        this.borderRadius = borderRadius;
-        return this;
-    }
-
-    @Override
-    public SScrollPane background(final IColor bg) {
-        this.bg = bg;
-        return this;
-    }
-
-    @Override
-    public SScrollPane foreground(final IColor fg) {
-        this.fg = fg;
-        return this;
-    }
-
-    @Override public SScrollPane grounds(final IColor bg, final IColor fg) {
-        this.bg = bg;
-        this.fg = fg;
-        return this;
-    }
+    @Override public SScrollPane borderRadius(final RRunnable<Integer> borderRadius) { this.borderRadius = borderRadius; return this; }
+    @Override public SScrollPane background(final IColor bg) { this.bg = bg; return this; }
+    @Override public SScrollPane foreground(final IColor fg) { this.fg = fg; return this; }
+    @Override public SScrollPane grounds(final IColor bg, final IColor fg) { this.bg = bg; this.fg = fg; return this; }
 
     @Override public SScrollPane update() {
         sx = Math.min(Math.max(sx, getWidth() - content.width()), 0);
         sy = Math.min(Math.max(sy, getHeight() - content.height()), 0);
+        final boolean o = bg.alpha() == 255 && borderRadius.run() <= 0;
+        if (isOpaque() != o)
+            setOpaque(o);
         repaint();
         return this;
     }
