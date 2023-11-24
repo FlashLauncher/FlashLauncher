0a1353c2c70309063765c1ec65c550b12a2fb0e0
illa4257 <48959534+illa4257@users.noreply.github.com>
2023-11-24 09:57:14 +0200

    Add method home(IAccount) : File

--- src/Launcher/base/IProfile.java
+++ src/Launcher/base/IProfile.java
@@ -5,6 +5,8 @@ import UIL.Lang;
 import UIL.LangItem;
 import UIL.base.IImage;
 
+import java.io.File;
+
 public interface IProfile {
     LangItem LANG_REMOVE = Lang.get("launcher.remove");
 
@@ -12,6 +14,11 @@ public interface IProfile {
 
     default boolean isCompatible(final IAccount account) { return true; }
     default IImage getIcon() { return null; }
+
+    /**
+     * @since FlashLauncher 0.2.2
+     */
+    default File home(final IAccount account) { return null; }
 
     void open(final IEditorContext context);
 
