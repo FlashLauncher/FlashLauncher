010e99c386f56c0ef49d5611fb4318beec385089
illa4257 <48959534+illa4257@users.noreply.github.com>
2023-12-10 08:34:12 +0200

    Update CIMeta.java

--- src/Launcher/CIMeta.java
+++ src/Launcher/CIMeta.java
@@ -1,17 +1,18 @@
 package Launcher;
 
 import UIL.base.IImage;
+import Utils.SyncVar;
 import Utils.Version;
 
 /**
  * @since FlashLauncher 0.2.4
  */
 public class CIMeta extends InstalledMeta {
-    private final ThreadLocal<IImage> icon = new ThreadLocal<>();
+    private final SyncVar<IImage> icon = new SyncVar<>();
 
-    private final ThreadLocal<Object>
-            name = new ThreadLocal<>(),
-            sd = new ThreadLocal<>();
+    private final SyncVar<Object>
+            name = new SyncVar<>(),
+            sd = new SyncVar<>();
 
     /**
      * @since FlashLauncher 0.2.4
@@ -27,4 +28,10 @@ public class CIMeta extends InstalledMeta {
     public void setIcon(final IImage icon) { this.icon.set(icon); }
     public void setName(final Object name) { this.name.set(name); }
     public void setShortDescription(final Object shortDescription) { sd.set(shortDescription); }
+
+    @Override
+    public String toString() {
+        final Object n = name.get();
+        return n == null ? null : n.toString();
+    }
 }
