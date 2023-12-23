622eab18e7483932d0b878ef73dd8f37746a7bd9
illa4257 <48959534+illa4257@users.noreply.github.com>
2023-12-23 22:44:47 +0200

    Mark

--- src/Launcher/Market.java
+++ src/Launcher/Market.java
@@ -6,7 +6,12 @@ import UIL.base.IImage;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public abstract class Market {
-    public static final int MENU_ICON_SIZE = 118, MENU_ICON_SIZE_MIN = 96;
+    public static final int MENU_ICON_SIZE = 118;
+
+    /**
+     * @since FlashLauncher 0.2.4
+     */
+    public static final int MENU_ICON_SIZE_MIN = 96;
 
     final ConcurrentLinkedQueue<Object> cl = new ConcurrentLinkedQueue<>();
 
