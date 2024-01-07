9cdfd87f1183b082f5dd1e0c1f53a33dadaa1117
illa4257 <48959534+illa4257@users.noreply.github.com>
2024-01-07 22:00:09 +0200

    Update IniGroup.java

--- src/Utils/IniGroup.java
+++ src/Utils/IniGroup.java
@@ -1,13 +1,18 @@
 package Utils;
 
+import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
-public class IniGroup {
+/**
+ * @since FlashLauncher 0.2.6
+ */
+public class IniGroup extends ListMap<String, Object> {
     public static final String[] COMMENTS = new String[] { ";", "#" };
     public static final char[] EQUALS = new char[] { '=', ':' };
-
-    private final ListMap<String, Object> values = new ListMap<>();
 
     public IniGroup() {}
     public IniGroup(final String data, final boolean allowNSubGroups) {
@@ -41,14 +46,14 @@ public class IniGroup {
         }
     }
 
-    public boolean has(final String k) { return values.containsKey(k); }
-    public Object get(final String k) { return values.get(k); }
-    public Set<String> keys() { return values.keySet(); }
-    public Set<Map.Entry<String, Object>> entrySet() { return values.entrySet(); }
+    /**
+     * @since FlashLauncher 0.2.6
+     */
+    public IniGroup(final Map<String, Object> map) { super(map); }
 
     public IniGroup newGroup(final String k) {
         final IniGroup g = new IniGroup();
-        values.put(k, g);
+        put(k, g);
         return g;
     }
 
@@ -60,7 +65,7 @@ public class IniGroup {
     public IniGroup group(final String k) {
         IniGroup g = getAsGroup(k);
         if (g == null)
-            values.put(k, g = new IniGroup());
+            put(k, g = new IniGroup());
         return g;
     }
 
@@ -80,13 +85,10 @@ public class IniGroup {
         return v.equals("1") || v.equals("true") || (!v.equals("0") && !v.equals("false") && defValue);
     }
 
-    public void put(final String k, final String v) { values.put(k, v); }
-    public void remove(final String k) { values.remove(k); }
-
     @Override
     public String toString() {
         final StringBuilder b = new StringBuilder();
-        final Set<Map.Entry<String, Object>> el = values.entrySet();
+        final Set<Map.Entry<String, Object>> el = entrySet();
         for (final Map.Entry<String, Object> e : el) {
             final Object v = e.getValue();
             if (v != null && !(v instanceof IniGroup))
@@ -95,7 +97,7 @@ public class IniGroup {
         for (final Map.Entry<String, Object> e : el) {
             final Object v = e.getValue();
             final Set<Map.Entry<String, Object>> l;
-            if (!(v instanceof IniGroup) || (l = ((IniGroup) v).values.entrySet()).isEmpty())
+            if (!(v instanceof IniGroup) || (l = ((IniGroup) v).entrySet()).isEmpty())
                 continue;
             boolean add = true;
             for (final Map.Entry<String, Object> en : l)
@@ -109,4 +111,221 @@ public class IniGroup {
         }
         return b.length() > 0 ? b.deleteCharAt(b.length() - 1).toString() : "";
     }
+
+    /**
+     * {@inheritDoc}
+     * @since FlashLauncher 0.2.6
+     */
+    @Override
+    public int size() {
+        return super.size();
+    }
+
+    /**
+     * {@inheritDoc}
+     * @since FlashLauncher 0.2.6
+     */
+    @Override
+    public boolean isEmpty() {
+        return super.isEmpty();
+    }
+
+    /**
+     * {@inheritDoc}
+     * @since FlashLauncher 0.2.6
+     */
+    @Override
+    public boolean containsKey(final Object key) {
+        return super.containsKey(key);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @since FlashLauncher 0.2.6
+     */
+    @Override
+    public boolean containsValue(final Object value) {
+        return super.containsValue(value);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @since FlashLauncher 0.2.6
+     */
+    @Override
+    public Object get(final Object key) {
+        return super.get(key);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @since FlashLauncher 0.2.6
+     */
+    @Override
+    public Object put(final String key, final Object value) {
+        return super.put(key, value);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @since FlashLauncher 0.2.6
+     */
+    @Override
+    public Object remove(final Object key) {
+        return super.remove(key);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @since FlashLauncher 0.2.6
+     */
+    @Override
+    public void putAll(final Map m) {
+        super.putAll(m);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @since FlashLauncher 0.2.6
+     */
+    @Override
+    public void clear() {
+        super.clear();
+    }
+
+    /**
+     * {@inheritDoc}
+     * @since FlashLauncher 0.2.6
+     */
+    @Override
+    public Set<String> keySet() {
+        return super.keySet();
+    }
+
+    /**
+     * {@inheritDoc}
+     * @since FlashLauncher 0.2.6
+     */
+    @Override
+    public Collection<Object> values() {
+        return super.values();
+    }
+
+    /**
+     * {@inheritDoc}
+     * @since FlashLauncher 0.2.6
+     */
+    @Override
+    public Set<Entry<String, Object>> entrySet() {
+        return super.entrySet();
+    }
+
+    /**
+     * {@inheritDoc}
+     * @since FlashLauncher 0.2.6
+     */
+    @Override
+    public Object getOrDefault(final Object key, final Object defaultValue) {
+        return super.getOrDefault(key, defaultValue);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @since FlashLauncher 0.2.6
+     */
+    @Override
+    public void forEach(final BiConsumer<? super String, ? super Object> action) {
+        super.forEach(action);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @since FlashLauncher 0.2.6
+     */
+    @Override
+    public void replaceAll(final BiFunction<? super String, ? super Object, ?> function) {
+        super.replaceAll(function);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @since FlashLauncher 0.2.6
+     */
+    @Override
+    public Object putIfAbsent(final String key, final Object value) {
+        return super.putIfAbsent(key, value);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @since FlashLauncher 0.2.6
+     */
+    @Override
+    public boolean remove(final Object key, final Object value) {
+        return super.remove(key, value);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @since FlashLauncher 0.2.6
+     */
+    @Override
+    public boolean replace(final String key, final Object oldValue, final Object newValue) {
+        return super.replace(key, oldValue, newValue);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @since FlashLauncher 0.2.6
+     */
+    @Override
+    public Object replace(final String key, final Object value) {
+        return super.replace(key, value);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @since FlashLauncher 0.2.6
+     */
+    @Override
+    public Object computeIfAbsent(final String key, final Function<? super String, ?> mappingFunction) {
+        return super.computeIfAbsent(key, mappingFunction);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @since FlashLauncher 0.2.6
+     */
+    @Override
+    public Object computeIfPresent(final String key, final BiFunction<? super String, ? super Object, ?> remappingFunction) {
+        return super.computeIfPresent(key, remappingFunction);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @since FlashLauncher 0.2.6
+     */
+    @Override public Object compute(final String key, final BiFunction<? super String, ? super Object, ?> remappingFunction) {
+        return super.compute(key, remappingFunction);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @since FlashLauncher 0.2.6
+     */
+    @Override public Object merge(final String key, final Object value, final BiFunction<? super Object, ? super Object, ?> remappingFunction) {
+        return super.merge(key, value, remappingFunction);
+    }
+
+    /**
+     * @deprecated FlashLauncher 0.2.6
+     * <p> Use {@link IniGroup#containsKey(Object)} instead.
+     */
+    public boolean has(final String k) { return containsKey(k); }
+
+    /**
+     * @deprecated FlashLauncher 0.2.6
+     * <p> Use {@link IniGroup#keySet()} instead.
+     */
+    public Set<String> keys() { return keySet(); }
 }
